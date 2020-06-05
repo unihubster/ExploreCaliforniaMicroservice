daa70c0e7e44f70df79b037622b0d479f99d278a
unihubster <var@var>
2020-06-05 03:21:32 +0300

    initial commit

---
+++ src/main/java/net/demo/exploreca/ExploreCaliforniaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package net.demo.exploreca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliforniaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreCaliforniaMicroserviceApplication.class, args);
+	}
+
+}
